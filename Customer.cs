af9d02fa23671f2ddf201609c250d3d659cb3dc1
aryark <aryark3112@gmail.com>
2023-07-26 10:02:50 +0530

    first commit

---
+++ Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Customer
+    {
+        int id;
+        string name;
+        DateTime mEndDate;
+        public Customer()
+        {
+            Console.WriteLine("Customer Constructor Call");
+            id = -1;
+            name = "not given";
+            mEndDate = DateTime.Now;
+        }
+        public int Id { get { return id; } set { id = value; } }
+        public string Name { get { return name; } set { name = value; } }
+        public DateTime MEndDate { get { return mEndDate; } set { mEndDate = value; } }
+    }
+}
+//Properties
+//[A.S][Mod.][DataType] PropertyName {get;set;}
+//get : reading
+//set : writing
+//1. ReadOnly get
+//2. WriteOnly set
+//3. Read and Write get,set
+//abstract
+//static
+//autoimplemented
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            Console.WriteLine("*** Default Values");
+            Console.WriteLine($"Customer ID :{customer.Id} Name: {customer.Name} MembershipEndDate{customer.MEndDate}");
+            Console.WriteLine("Enter Customer ID");
+            customer.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Customer name");
+            customer.Name = Console.ReadLine();
+            Console.WriteLine("Enter Customer Membership End Date");
+            customer.MEndDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Stored Record As Follows!!!");
+            Console.WriteLine($"Customer ID :{customer.Id} Name: {customer.Name} MembershipEndDate {customer.MEndDate}" );
+            Console.ReadKey();
+        }
+    }
+}
